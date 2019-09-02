75724327ef0adbaa4c47281e4a192577dc0a51c0
shmohamud <shmohamud@email.wm.edu>
2019-09-02 02:17:10 -0400

    complete

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,24 @@
 export default function createStore(reducer) {
-  // add your code here
+
+  let state;
+
+  let getState = () => {
+    return state
+  }
+  
+  let dispatch = (action) => {
+    state = reducer(state, action)
+  }
+
+let INIT = {type:"INTIALIZE"}
+
+dispatch(INIT)
+
+  return {getState, dispatch}
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+
